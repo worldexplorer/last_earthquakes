c6367312338934fbe87a90fcd83ae46036e4bbae
worldexplorer <user@pi10>
2023-02-19 20:42:14 +0300

    fails om run

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,42 @@
+import 'package:bloc_rest_api/bloc_rest_api.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'network/dto/feature_collection_dto.dart';
 import 'views/loader.dart';
 
 void main() {
+  // Specify the base url
+  // ApiConfig.baseUrl = "https://jsonplaceholder.typicode.com/";
+  // Specify base header for response
+  // ApiConfig.header = {"Content-Type": "application/json"};
+  // runApp(const App());
   runApp(const MyApp());
+}
+
+// https://pub.dev/packages/bloc_rest_api/example
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of the application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => RequestCubit<FeatureCollectionDto>(
+            fromMap: (json) => FeatureCollectionDto.fromJson(json),
+          ),
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/views/loader.dart
+++ lib/views/loader.dart
@@ -1,7 +1,10 @@
-import 'package:LastHourEarthquakes/network/fetch_1wk_earthquakes.dart';
+import 'package:bloc_rest_api/bloc_rest_api.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../network/fetch_1hr_earthquakes.dart';
+import '../network/dto/feature_collection_dto.dart';
+import '../network/fetch_1hr_earthquakes.dart';
+import '../network/fetch_1wk_earthquakes.dart';
 import 'tabs.dart';
 
 class Loader extends StatelessWidget {
@@ -10,26 +13,56 @@ class Loader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: Future.wait([fetch1hrEarthquakes(), fetch1wkEarthquakes()]),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('Error loading Earthquakes:\n\n${snapshot.error}'),
-            );
-          } else if (snapshot.hasData) {
-            return Tabs(
-              hourly_all: snapshot.data![0].features,
-              hourlyFetcher: fetch1hrEarthquakes,
-              weekly_45mag: snapshot.data![1].features,
-              weeklyFetcher: fetch1wkEarthquakes,
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
+      body: BlocConsumer<RequestCubit<FeatureCollectionDto>,
+          RequestState<FeatureCollectionDto>>(
+        listener:
+            (BuildContext context, RequestState<FeatureCollectionDto> state) {
+          if (state.status == RequestStatus.failure) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.errorMessage ?? 'UNKNOWN_FAILURE')),
             );
           }
         },
+        builder: (context, state) {
+          switch (state.status) {
+            case RequestStatus.empty:
+              return const Center(
+                  child: Text("Press the button to get some data"));
+
+            case RequestStatus.loading:
+              return const Center(child: CircularProgressIndicator());
+
+            case RequestStatus.success:
+              return Tabs(
+                hourly_all: state.model!.features,
+                hourlyFetcher: fetch1hrEarthquakes,
+                weekly_45mag: const [],
+                weeklyFetcher: fetch1wkEarthquakes,
+              );
+
+            case RequestStatus.failure:
+              return Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Text(state.model.toString()),
+                ),
+              );
+
+            default:
+              return Container();
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context
+              .read<RequestCubit<FeatureCollectionDto>>()
+              .request(fetch1hrEarthquakes());
+          // context.read<RequestCubit<PostModel>>().getRequest(
+          //   handle: 'posts/1',
+          // );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
